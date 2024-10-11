2615e637badddf57a7948ee2348a1f81f1111cbd
Tai Jones <69367619+ProgramJones@users.noreply.github.com>
2024-10-11 04:09:38 -0400

    Adding the initial files.

---
+++ src/script/managers.js
@@ -0,0 +1,26 @@
+class ProjectManager {
+  #allProjects = {};
+  #projects = [];
+
+  sortProjects() {}
+
+  filterProjects() {}
+
+  viewProjects() {}
+
+  createProject() {}
+
+  deleteProject() {}
+
+  updateProject() {}
+}
+
+class GroupManager {
+  #groups = [];
+
+  createCreateOption() {}
+
+  deleteGroupOption() {}
+
+  viewGroupOptions() {}
+}
---
+++ src/script/projectTypes.js
@@ -0,0 +1,28 @@
+function project() {
+  let screenshot;
+  let title = "";
+  let createdTime; // Make const initialized to current Date time
+  let lastEditedTime;
+  let groups = [];
+  let isFavorite = false;
+  let content;
+
+  function getTitle() {}
+  function setTitle() {}
+
+  function getCreatedTime() {}
+
+  function getLastEditedTime() {}
+  function setLastEditedTime() {}
+
+  function getGroups() {}
+  function setGroups() {}
+
+  function getIsFavorite() {}
+  function setIsFavorite() {}
+
+  function getContent() {}
+  function setContent() {}
+}
+
+function note() {}
